7d6e7ee9e7dd790e5f62371cf65166a4874b3243
JabelFontoura <jabel_fontoura@hotmail.com>
2017-02-22 18:37:55 -0300

    Primeira conexao com o banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,21 @@
 module.exports = function(app){
 	app.get('/produtos', function(request, response){
 		var pg = require('pg');
-		var connection = 
-		response.render("produtos/lista");
+		var conStrig = "postgres://postgres:jabel@localhost/CasaDoCodigo";
+
+		pg.connect(conStrig, function(err, client, done){
+			if(err){
+				return console.error("Error fetching client from pool", err);
+			}
+			console.log("Conctado com o banco");
+			client.query('SELECT * FROM livros', function(err, result) {
+			    done();
+			    if (err) {
+			      return console.error('Error running query', err);
+			    }
+			    response.send(result.rows);
+			  });
+
+		});
 	});
 }
