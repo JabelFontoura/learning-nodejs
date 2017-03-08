f00fe4f8b8852f4a7b8ec7e9c20ef9463f16b5ca
JabelFontoura <jabel_fontoura@hotmail.com>
2017-03-08 18:49:04 -0300

    Criando testes iniciais com mocha

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,24 @@
+var http = require('http');
+
 describe('#ProdutosController', function(){
-	it('#listagem json', function(){
-		console.log('Teste de verificação de listagem json');
+	it('#listagem json', function(done){
+		var config = {
+			hostaname: 'localhost',
+			port: 3000,
+			path:'/produtos',
+			headers: {
+				'Accept': 'application/json'
+			}
+		};
+
+		http.get(config, function(response) {
+			if(response.statusCode == 200){
+				console.log("Status Ok");
+			}
+			if(response.headers['content-type'] == 'application/json; charset=utf-8'){
+				console.log("Content-type Ok ");
+			}
+			done();
+		});
 	});
 });
