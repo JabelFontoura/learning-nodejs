cfe4c53209acf93e065653b255da434d58c1f604
JabelFontoura <jabel_fontoura@hotmail.com>
2017-03-10 14:10:49 -0300

    Implementação de promoções com socket.io

--- app/routes/promocoes.js
+++ app/routes/promocoes.js
@@ -1,10 +1,16 @@
 module.exports = function(app) {
 	app.get('/promocoes/form', function(request, response) {
-		var client = app.infra.connectionFacoty();
+		var client = app.infra.connectionFactory();
 		var produtosDAO = new app.infra.ProdutosDAO(client);
 
 		produtosDAO.lista(function(error, result) {
-			response.render('/promocoes', {lista:result.rows});
+			response.render('promocoes/form', {lista:result.rows});
 		});
+	});
+
+	app.post('/promocoes', function(request, response) {
+		var promocao = request.body;
+		app.get('io').emit('novaPromocao', promocao);
+		response.redirect('promocoes/form');
 	});
 }
