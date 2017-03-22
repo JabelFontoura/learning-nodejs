9950a21613a013240bf8cac7af2f1862cce774e2
JabelFontoura <jabel_fontoura@hotmail.com>
2017-03-22 17:35:40 -0300

    Ajuste pastas

--- casadocodigo/app.js
+++
@@ -1,9 +0,0 @@
-var app = require('./config/express')();
-var http = require("http").Server(app);
-var io = require('socket.io')(http);
-
-app.set('io', io);
-
-http.listen(3000, function(){
-	console.log("Servidor online em localhost:3000");
-});
